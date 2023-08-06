5fe60ced47276ea2ce573e2a29e6b19445b3d274
Viktor Wysluch <v.wysluch19@gmail.com>
2023-08-06 23:17:44 +0200

    Skript + update von updateall.py
    Skript, mit welchem man auf updateall.py und repairwindows.py zugreifen kann

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+def main():
+    while True:
+        print("WindowsOptimizerTool:")
+        print("1. Update all Programs       winget")
+        print("2. Repair Windows            sfc /scannow + DISM + CHKDSK")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            os.system("python updateall.py")
+        elif choice == "2":
+            os.system("python repairwindows1.py")
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+if __name__ == "__main__":
+    main()
--- updateall.py
+++ updateall.py
@@ -1,4 +1,8 @@
 import os
 
-command = 'winget upgrade --accept-source-agreements --all --include-unknown --silent'
-os.system(command)
+def main():
+    command = 'winget upgrade --accept-source-agreements --all --include-unknown --silent'
+    os.system(command)
+
+if __name__ == "__main__":
+    main()
